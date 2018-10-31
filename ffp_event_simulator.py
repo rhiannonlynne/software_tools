541b88c9d9eac76d27de29b106bc662e1231bccd
rachel3834 <rstreet@lco.global>
2018-10-30 21:34:51 -0700

    Refining parameter selection

--- ffp_event_simulator.py
+++ ffp_event_simulator.py
@@ -9,21 +9,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os, sys
 from pyLIMA import microlsimulator, telescopes, microlmodels, event
-from pyLIMA import microlfits
+from pyLIMA import microlfits, microltoolbox
 import jplhorizons_utils
+import copy
 
 def simulate_ffp():
     
-    t0 = 2460346.500000
-    u0 = 0.001
+    spring = True
+    fall = False
+
+    u0 = 0.01
     tE = 1.0
-    start_jd = 2460331.500000
-    end_jd = 2460360.500000
+    if spring:
+        t0 = 2460394.400000
+        start_jd = 2460389.500000       # 2024 March 20
+        end_jd = 2460399.500000
+    if fall:
+        t0 = 2460389.500000
+        start_jd = 2460573.500000       # 2024 Aug 20
+        end_jd = 2460583.500000        # 2024 Aug 30
     ra = 270.0
     dec = -27.0
     piEN = 0.01
     piEE = 0.01
-    blend_ratio = 0.1
+    blend_ratio = 0.2
+    source_flux = mag_to_flux(22.0,1.0,24.0)
+    blend_flux = source_flux * blend_ratio
     
     model_params = [ t0, u0, tE, piEN, piEE ]
     
@@ -33,13 +44,18 @@ def simulate_ffp():
     wfirst_aperture = 2.4
     wfirst_read_noise = 10.0
     
+    if spring:
+        horizons_file = 'wfirst_observer_table_spring.txt'
+    if fall: 
+        horizons_file = 'wfirst_observer_table_fall.txt'
+        
     ffp_lsst = event.Event()
     ffp_lsst.name = 'FFP'
     ffp_lsst.ra = ra
     ffp_lsst.dec = dec
     
     lsst_lc = simulate_lightcurve(start_jd, end_jd, 0.5/24.0, lsst_aperture, 
-                                  day_gaps=True)
+                                  spring, fall, day_gaps=True )
     lsst = telescopes.Telescope(name='LSST', camera_filter='i', 
                                location='Earth',
                                light_curve_magnitude=lsst_lc)
@@ -57,18 +73,21 @@ def simulate_ffp():
     ffp_lsst.fits.append(fit_lsst)
     
     print('Generated event lightcurve from LSST')
+    print('Model parameters:')
+    print_fit_params(model_params)
+    
     
     ffp_wfirst = event.Event()
     ffp_wfirst.name = 'FFP'
     ffp_wfirst.ra = ra
     ffp_wfirst.dec = dec
     
-    horizons_table = jplhorizons_utils.parse_JPL_Horizons_table(horizons_file_path='wfirst_observer_table.txt', 
+    horizons_table = jplhorizons_utils.parse_JPL_Horizons_table(horizons_file_path=horizons_file, 
                                                                 table_type='OBSERVER')
                                                                 
     spacecraft_positions = jplhorizons_utils.extract_spacecraft_positions(horizons_table,t0)
         
-    wfirst_lc = simulate_lightcurve(start_jd, end_jd, 0.25/24.0, wfirst_aperture)
+    wfirst_lc = simulate_lightcurve(start_jd, end_jd, 0.25/24.0, wfirst_aperture, spring, fall)
         
     wfirst = telescopes.Telescope(name='WFIRST', camera_filter='W149', 
                                spacecraft_name = 'WFIRST',
@@ -90,11 +109,13 @@ def simulate_ffp():
     ffp_wfirst.fits.append(fit_wfirst)
     
     print('Generated event lightcurve from WFIRST')
+    print('Model parameters:')
+    print_fit_params(fit_wfirst)
     
     parameters = [ t0, u0, tE ]
 
-    lsst_pylima_params = extract_event_parameters(ffp_lsst, fit_lsst, parameters, blend_ratio)
-    wfirst_pylima_params = extract_event_parameters(ffp_wfirst, fit_wfirst, parameters, blend_ratio)
+    lsst_pylima_params = extract_event_parameters(ffp_lsst, fit_lsst, parameters, source_flux, blend_ratio)
+    wfirst_pylima_params = extract_event_parameters(ffp_wfirst, fit_wfirst, parameters, source_flux, blend_ratio)
     
     lsst_lc = add_lensing_event_to_lightcurve(lsst_pylima_params, ffp_lsst, 
                                               fit_lsst, lsst_read_noise)
@@ -106,9 +127,7 @@ def simulate_ffp():
                             'LSST', 'WFIRST', 'ffp_sim_lightcurve.png',
                             t0=t0,tE=tE)
     
-def extract_event_parameters(lens, fit, event_params, blend_ratio):
-        
-    source_flux = lens.telescopes[0].lightcurve_magnitude[:,1].min()
+def extract_event_parameters(lens, fit, event_params, source_flux, blend_ratio):
     
     parameters = event_params + [ source_flux, blend_ratio ]
     
@@ -131,7 +150,8 @@ def add_lensing_event_to_lightcurve(pylima_params, lens, fit, read_noise):
     
     return lightcurve
     
-def simulate_lightcurve(start_jd, end_jd, cadence, aperture, day_gaps=False):
+def simulate_lightcurve(start_jd, end_jd, cadence, aperture, spring, fall,
+                        day_gaps=False):
     """Function to generate lightcurve data
     Start and end times in JD, cadence in days^-1    
     """
@@ -142,12 +162,25 @@ def simulate_lightcurve(start_jd, end_jd, cadence, aperture, day_gaps=False):
         
         dec_days = ts % 1
         
-        idx = np.where(dec_days >= 9.0/24.0)[0]
-        jdx = np.where(dec_days <= 11.5/24.0)[0]
-        kdx = list(set(idx).intersection(set(jdx)))
-        
+        if spring:
+            
+            idx = np.where(dec_days >= 6.4/24.0)[0]
+            jdx = np.where(dec_days <= 11.0/24.0)[0]
+            kdx = list(set(idx).intersection(set(jdx)))
+            
+        if fall:
+
+            idx1 = np.where(dec_days >= 21.5/24.0)[0]
+            jdx1 = np.where(dec_days <= 23.9/24.0)[0]
+            idx2 = np.where(dec_days >= 0.01/24.0)[0]
+            jdx2 = np.where(dec_days <= 3.0/24.0)[0]
+
+            kdx1 = list(set(idx1).intersection(set(jdx1)))
+            kdx2 = list(set(idx2).intersection(set(jdx2)))
+            kdx = kdx1+kdx2
+            
         ts = ts[kdx]
-        
+    
     lightcurve = np.zeros((len(ts),3))
 
     lightcurve[:,0] = ts
@@ -156,7 +189,7 @@ def simulate_lightcurve(start_jd, end_jd, cadence, aperture, day_gaps=False):
     fluxes = mag_to_flux(lightcurve[:,1], 1.0, 24.0)
     
     (lightcurve[:,2], read_noise, possion_noise) = calc_phot_uncertainty(fluxes,10.0)
-    
+
     return lightcurve
     
 def mag_to_flux(mag, gain, ZP):
@@ -241,10 +274,10 @@ def plot_fitted_lightcurves(lc1, lc2, event1, event2, label1, label2, file_path,
         
         model_lc2 = generate_model_lightcurve(event2)
         
-        plt.plot(ts1,model1,linestyle='dashed',
+        plt.plot(ts1,model_lc1,linestyle='dashed',
                      color='#4c1377')
          
-        plt.plot(ts2,model2,linestyle='solid',
+        plt.plot(ts2,model_lc2,linestyle='solid',
                      color='black')
         
     plt.xlabel('HJD - '+str(dt), fontsize=18)
@@ -270,7 +303,50 @@ def plot_fitted_lightcurves(lc1, lc2, event1, event2, label1, label2, file_path,
 
     plt.close(1)
 
+def generate_model_lightcurve(e):
+    """Function to produce a model lightcurve based on a parameter set
+    fitted by pyLIMA
+    
+    Inputs:
+    e  Event object
+    
+    """
+    
+    lc = e.telescopes[0].lightcurve_magnitude
+    
+    fit_params = e.fits[-1].model.compute_pyLIMA_parameters(e.fits[-1].fit_results)
+    
+    ts = np.linspace(lc[:,0].min(), lc[:,0].max(), len(lc[:,0]))
 
+    reference_telescope = copy.copy(e.fits[-1].event.telescopes[0])
+    
+    reference_telescope.lightcurve_magnitude = np.array([ts, [0] * len(ts), [0] * len(ts)]).T
+    
+    reference_telescope.lightcurve_flux = reference_telescope.lightcurve_in_flux()
+
+    if e.fits[-1].model.parallax_model[0] != 'None':
+        
+        reference_telescope.compute_parallax(e.fits[-1].event, e.fits[-1].model.parallax_model)
+    
+    print_fit_params(fit_params)
+    
+    flux_model = e.fits[-1].model.compute_the_microlensing_model(reference_telescope, fit_params)[0]
+    
+    mag_model = microltoolbox.flux_to_magnitude(flux_model)
+
+    return mag_model
+
+def print_fit_params(fit_params):
+    
+    key_list = ['to', 'uo', 'tE', 'piEN', 'piEE', 'rho',\
+                'fs_WFIRST', 'fs_LSST', 'g_WFIRST', 'g_LSST']
+                
+    for key in key_list:
+        try:
+            print(key, getattr(fit_params,key))
+        except AttributeError:
+            pass
+    
 if __name__ == '__main__':
     
     simulate_ffp()
